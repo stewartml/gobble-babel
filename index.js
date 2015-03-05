fa4003b5b28624ced68a044a206251c63f0f6329
Stewart MacKenzie-Leigh <stewart@stugo.co.uk>
2015-03-05 11:25:33 +0000

    Add 'options' field to transform options

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = babel;
 
 function babel ( code, options ) {
+	// allow options to be specified like: transform('babel', {ext: '.js', options: {/*babel stuff*/}})
+	if (options.options) options = options.options;
 	options.sourceMap = options.sourceMap !== false;
 	return require( 'babel-core' ).transform( code, options );
 }
